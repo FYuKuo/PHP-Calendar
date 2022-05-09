06f49b9aec706c02254a3a2b3c308ad2189ddfba
FYKuo <teresa99339@gmail.com>
2022-05-09 14:09:03 +0800

    加入月份前後的空白格子 , 新增表格上方的星期列表

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
     flex-wrap: wrap;
     align-items: flex-start;
     margin: 10px auto;
+    text-align: center;
   }
 
   .table>div {
@@ -21,6 +22,12 @@
     border: 1px solid lightgray;
     margin-left: -1px;
     margin-top: -1px;
+    line-height: 100px;
+  }
+
+  .table>.table_hd {
+    height: 50px;
+    line-height: 50px;
   }
    
 
@@ -29,7 +36,7 @@
 <h1>萬年曆</h1>
 <?php
 /*請在這裹撰寫你的萬年曆程式碼*/  
-$month=5; //月份
+$month=6; //月份
 $firstDay=date("Y-") . $month . "-1"; //月份第一天
 $firstDaySecond=strtotime($firstDay); //月份第一天轉秒
 $monthDay=date('t',$firstDaySecond); //月份天數
@@ -39,9 +46,21 @@ $firstDayWeek=date('w',$firstDaySecond); //第一天星期幾
 $lasttDayWeek=date('w',$lastDaySecond); //最後一天星期幾
 
 $allDay=[]; //要放所有天數的空陣列
+
+//月份第一天前的空日期
+for($i=0 ; $i<$firstDayWeek; $i++){
+  $allDay[]="";
+}
+
+//月份所有日期
 for($i=0 ; $i<$monthDay ; $i++) {
   $date=date('Y-m-d',strtotime("+$i days",$firstDaySecond));
   $allDay[]=$date;
+}
+
+//月份最後一天後的空日期
+for($i=0 ; $i<6-$lasttDayWeek ;$i++){
+  $allDay[]="";
 }
 
 // echo "<pre>";
@@ -50,6 +69,16 @@ for($i=0 ; $i<$monthDay ; $i++) {
 
 
 echo "<div class='table'>";
+
+echo "<div class='table_hd'>" . "SUN" ."</div>";
+echo "<div class='table_hd'>" . "MON" ."</div>";
+echo "<div class='table_hd'>" . "TUE" ."</div>";
+echo "<div class='table_hd'>" . "WED" ."</div>";
+echo "<div class='table_hd'>" . "THU" ."</div>";
+echo "<div class='table_hd'>" . "FRI" ."</div>";
+echo "<div class='table_hd'>" . "SAT" ."</div>";
+
+
 
 foreach($allDay as $day) {
   echo "<div>{$day}</div>";
