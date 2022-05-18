63cf578b4a14cac2e085e90fb75d940e5e483415
FY Kuo <teresa99339@gmail.com>
2022-05-18 23:26:27 +0800

    新增php判斷年份的區塊,加入年分,月份選單

--- index.php
+++ index.php
@@ -7,7 +7,26 @@
   <h1>萬年曆</h1>
   <?php
   /*請在這裹撰寫你的萬年曆程式碼*/
-  $month = 5; //月份
+
+  //判斷網址內是否有參數,有的話讓month,year去抓取資料,沒有的話就是目前月份和年
+  if (isset($_GET['year']) && isset($_GET['month'])) {
+    $year = $_GET['year'];
+    $month = $_GET['month'];
+  } elseif (isset($_GET['month']) && !isset($_GET['year'])) {
+    $month = $_GET['month'];
+    $year = date('Y');
+  } elseif (isset($_GET['year']) && !isset($_GET['month'])) {
+    $year = $_GET['year'];
+    $month = date('m');
+  } else {
+    $month = date('m');
+    $year = date('Y');
+  }
+
+  //上一年和下一年的變數設定
+  $lastYear = $year - 1;
+  $nextYear = $year + 1;
+
   $firstDay = date("Y-") . $month . "-1"; //月份第一天
   $firstDaySecond = strtotime($firstDay); //月份第一天轉秒
   $monthDay = date('t', $firstDaySecond); //月份天數
@@ -16,7 +35,6 @@
   $firstDayWeek = date('w', $firstDaySecond); //第一天星期幾
   $lasttDayWeek = date('w', $lastDaySecond); //最後一天星期幾
   $monthFont = date('F', $firstDaySecond); //月份英文
-  $yearFont = date('Y', $firstDaySecond); //月份的年
 
   $allDay = []; //要放所有天數的空陣列
 
@@ -36,9 +54,9 @@
     $allDay[] = "";
   }
 
-  // echo "<pre>";
-  // print_r($allDay);
-  // echo "</pre>";
+  //所有月份的英文陣列,並設定索引從1開始
+  $allMonth = ['1' => 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
   $today = date('Y-m-d');
   $todayWeek = date('l', strtotime($today));
   $todayFont = date('d', strtotime($today));
@@ -73,41 +91,82 @@
       </div>
     </div>
 
+    <!-- 月曆的右半邊 -->
+    <div class="right">
 
-    <!-- 日期格子區 -->
-    <div class="table">
+      <!-- 日期格子區 -->
+      <div class="table">
 
-      <div class="weekDay">
-        <div class="weekend">SUN</div>
-        <div class="workDay">MON</div>
-        <div class="workDay">TUE</div>
-        <div class="workDay">WED</div>
-        <div class="workDay">THU</div>
-        <div class="workDay">FRI</div>
-        <div class="weekend">SAT</div>
+        <!-- 日期區的最上面 選擇年分 -->
+        <div class="header_year">
+
+          <!-- 前一年 -->
+          <div class="year-page">
+            <a href="./index.php?year=<?= $lastYear ?>&month=<?= $month ?>">◀</a>
+          </div>
+
+          <!-- 今年 -->
+          <div class="year" id="yearNow">
+            <?= $year ?>
+          </div>
+
+          <!-- 下一年 -->
+          <div class="year-page">
+            <a href="./index.php?year=<?= $nextYear ?>&month=<?= $month ?>">▶</a>
+          </div>
+        </div>
+
+        <!-- 日期區選擇月份 -->
+        <div class="header_month">
+          <?php
+
+          //印出所有月份的英文
+          foreach ($allMonth as $monthNumbr => $months) {
+
+            //檢查是不是參數目前月份 是的話就在div放上class改顏色
+            $checkmonth = "";
+            if ($monthFont == $months) {
+              $checkmonth = "thisMonth";
+            }
+
+            echo "<div><a class='$checkmonth' href='./index.php?year=$year&month=$monthNumbr'>{$months}</a></div>";
+          }
+          ?>
+        </div>
+
+        <!-- 日期區的星期 -->
+        <div class="weekDay">
+          <div class="weekend">SUN</div>
+          <div class="workDay">MON</div>
+          <div class="workDay">TUE</div>
+          <div class="workDay">WED</div>
+          <div class="workDay">THU</div>
+          <div class="workDay">FRI</div>
+          <div class="weekend">SAT</div>
+        </div>
+
+        <?php
+        //利用foreach迴圈印出所有天數
+        foreach ($allDay as $day) {
+
+          //檢查是不是今天
+          $checktoday = "";
+          if ($day == $today) {
+            $checktoday = "today";
+          }
+
+          //如果day裡面不是空的就轉換日期格式為'天'
+          if (!empty($day)) {
+
+            $dateFont = date('d', strtotime($day));
+            echo "<div class='dateTd'><div class='dateBG $checktoday'>{$dateFont}</div></div>";
+          } else {
+            echo "<div class='dateTd'></div>";
+          }
+        }
+        ?>
+
       </div>
-
-      <?php
-      //利用foreach迴圈印出所有天數
-      foreach ($allDay as $day) {
-
-        //檢查是不是今天
-        $checktoday = "";
-        if ($day == $today) {
-          $checktoday = "today";
-        }
-
-        //如果day裡面不是空的就轉換日期格式為'天'
-        if (!empty($day)) {
-
-          $dateFont = date('d', strtotime($day));
-          echo "<div class='dateTd'><div class='dateBG $checktoday'>{$dateFont}</div></div>";
-        } else {
-          echo "<div class='dateTd'></div>";
-        }
-      }
-      ?>
-
     </div>
   </div>
 
